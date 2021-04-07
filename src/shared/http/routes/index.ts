5aeebc8ac34d9ca9c959bc8f2548b428d6078955
pgabrieldeveloper <g4brieldev@gmail.com>
2021-04-07 09:35:29 -0300

    adding middleware of  authentication

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -3,12 +3,13 @@ import spentRouter from '../../../modules/spent/routes/spent.route';
 import gainRouter from '../../../modules/gain/routes/gain.routes';
 import userRoutes from '../../../modules/user/routes/user.routes';
 import walletRoutes from '../../../modules/wallet/routes/wallet.routes';
+import isAuthenticate from '../../../modules/user/middleware/isAuthenticate';
 const routes = Router();
 routes.get('/', (req, res) => {
   return res.json({ message: 'Olá express' });
 });
-routes.use('/wallet', walletRoutes);
+routes.use('/wallet', isAuthenticate, walletRoutes);
 routes.use('/user', userRoutes);
-routes.use('/gain', gainRouter);
-routes.use('/spent', spentRouter);
+routes.use('/gain', isAuthenticate, gainRouter);
+routes.use('/spent', isAuthenticate, spentRouter);
 export default routes;
