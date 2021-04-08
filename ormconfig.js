fbda5eb4eda8574fd15dc4ec37787714667a4119
pgabrieldeveloper <g4brieldev@gmail.com>
2021-04-08 10:28:57 -0300

    change build for dist

--- ormconfig.js
+++ ormconfig.js
@@ -1,8 +1,8 @@
 module.exports = {
   type: 'postgres',
   url: process.env.DATABASE_URL,
-  entities: ['./build/modules/**/typeorm/entities/*.js'],
-  migrations: ['./build/shared/typeorm/migrations/*.js'],
+  entities: ['./dist/modules/**/typeorm/entities/*.js'],
+  migrations: ['./dist/shared/typeorm/migrations/*.js'],
   cli: {
     migrationsDir: './src/shared/typeorm/migrations',
   },
